2bae96fb75e287c7a48fd4a748d13b0bdd8e1ccd
NeoMix1990 <NeoMix775@gmail.com>
2018-05-19 18:54:01 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,12 @@ export class AppComponent implements OnInit {
 	constructor() {
 	}
 
-	sec = 0;
-	min = 0;
-	hour = 0;
+	startButton: boolean = false;
+	stopButton: boolean = true;
+
+	sec = 55;
+	min = 59;
+	hour = 23;
 
 
 	ngOnInit() {
@@ -28,81 +31,64 @@ export class AppComponent implements OnInit {
 		sec: this.sec
 	};
 
-	start(hour, min, sec) {
-		if (hour >= 24) {
-			hour = 23;
-		};
-		if (min >= 60) {
-			min = 59;
-		};
-		if (sec >= 60) {
-			sec = 59;
-		};
-		if(this.ittimer.hour == 0) {
-			this.ittimer.hour = hour;
-		} else {
-			this.ittimer.hour;
-		}
-		if(this.ittimer.min == 0) {
-			this.ittimer.min = min;
-		} else {
-			this.ittimer.min;
-		}
-		if(this.ittimer.sec == 0) {
-			this.ittimer.sec = sec;
-		} else {
-			this.ittimer.sec;
-		}
+	start(ittimer) {
+		this.stopButton = false;
+		this.startButton = true;
+		
+		console.log(typeof (this.ittimer.hour));
 		console.log(this.ittimer.hour + ':' + this.ittimer.min + ':' + this.ittimer.sec);
 
-		if (this.ittimer.hour != 0 || this.ittimer.min != 0 || this.ittimer.sec != 0) {
-			this.mytimer = timer(1000, 1000)
-				.pipe(map(() => --this.ittimer.sec))
-				.subscribe((i) => {
-					this.ittimer.sec = i;
-					if (this.ittimer.sec === -1) {
-						this.ittimer.sec = 59;
-						this.ittimer.min--;
-					}
-					if (this.ittimer.min === -1) {
-						this.ittimer.min = 59;
-						this.ittimer.hour--;
-					}
-					if (this.ittimer.hour == 0 && this.ittimer.min == 0 && this.ittimer.sec == 0) {
-						return this.mytimer.unsubscribe();
-					}
-				});
-		}
+
+
+		this.mytimer = timer(1000, 1000)
+			.pipe(map(() => ++this.ittimer.sec))
+			.subscribe((i) => {
+				this.ittimer.sec = i;
+				if (this.ittimer.sec === 60) {
+					this.ittimer.sec = 0;
+					this.ittimer.min++;
+				}
+				if (this.ittimer.min === 60) {
+					this.ittimer.min = 0;
+					this.ittimer.hour++;
+				}
+				if(this.ittimer.hour == 24 && this.ittimer.min == 0 && this.ittimer.sec == 0) {
+					this.ittimer.hour = 0;
+					this.ittimer.min = 0;
+					this.ittimer.sec = 0;
+					this.stopButton = true;
+					this.startButton = false;
+					return this.mytimer.unsubscribe();
+				}
+				console.log(this.ittimer);
+			});
+
 
 	}
 
 	stop(ittimer) {
-		if (this.ittimer.hour == 0 && this.ittimer.min == 0 && this.ittimer.sec == 0) {
-			return ittimer;
-		} else {
-			return this.mytimer.unsubscribe();
+		this.stopButton = true;
+		this.startButton = false;
+		return this.mytimer.unsubscribe();
+
+	}
+
+	wait(ittimer) {
+		return this.mytimer.unsubscribe();
+	}
+
+	reset(ittimer) {
+		this.stopButton = true;
+		this.startButton = false;
+		if (this.ittimer.hour != 0) {
+			this.ittimer.hour = 0;
 		}
-	}
-
-	wait() {
-
-	}
-
-	reset(hour, min, sec) {
-		if (this.mytimer != undefined) {
-			if (this.ittimer.hour != 0) {
-				this.ittimer.hour = 0;
-			}
-			if (this.ittimer.min != 0) {
-				this.ittimer.min = 0;
-			}
-			if (this.ittimer.sec != 0) {
-				this.ittimer.sec = 0;
-			}
-			this.mytimer.unsubscribe();
+		if (this.ittimer.min != 0) {
+			this.ittimer.min = 0;
 		}
+		if (this.ittimer.sec != 0) {
+			this.ittimer.sec = 0;
+		}
+		this.mytimer.unsubscribe();
 	}
-
-
-	// console.log(this.timer);
 }
